70bd2c0c9b8b071127761350dc4d8f9c263975c6
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2024-06-01 16:13:07 +0200

    Fix filament access

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
@@ -44,8 +46,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-    
-    public function canAccessFilament(): bool
+
+    public function canAccessPanel(Panel $panel): bool
     {
         return true;
     }
