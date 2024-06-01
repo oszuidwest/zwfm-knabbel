b3cc170c9b22579c7b47b36daf6ade3a98f3e11a
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2024-06-01 14:23:53 +0200

    Add Socialite

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('azure')->redirect();
+});
+
+Route::get('/auth/callback', function () {
+    $user = Socialite::driver('azure')->user();
+
+    dd($user);
+    // $user->token
 });
