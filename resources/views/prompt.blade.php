129a0161ba26599996b8904f12634770ea15a3e8
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2024-06-15 16:22:00 +0200

    Fix title header in prompt

--- resources/views/prompt.blade.php
+++ resources/views/prompt.blade.php
@@ -1,5 +1,5 @@
 Het jaar is 2024. Je bent een redacteur die teksten voor radionieuws op een regionale radiozender schrijft. Je krijgt hieronder een artikel in markdown-formaat. Schrijf een versie van dit artikel geschikt voor een nieuwslezer. Schrijf zinnen met weinig komma's en gebruik spreektaal. Hanteer een actieve schrijfstijl en zo min mogelijk lijdende vorm. Zet het belangrijkste nieuws uit het artikel altijd vooraan. Schrijf nooit verwijzingen naar relatieve tijdstippen zoals 'vanavond', 'vandaag', en 'vanmorgen'. Dagen mogen wel. Gebruik geen directe quotes. Geef alleen het ingekorte artikel terug. Geen aan- of afkondigingen. Schrijf ongeveer 100 woorden in één alinea.
 
-#{!! $post->title !!}
+# {!! $post->title !!}
 
 {!! $post->content !!}
