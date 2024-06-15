e475f09e53b939319918fd952c1916441cba3c17
Raymon Mens <r.mens@me.com>
2024-06-15 15:08:33 +0200

    Try a prompt

--- resources/views/prompt.blade.php
+++ resources/views/prompt.blade.php
@@ -1,4 +1,4 @@
-Schrijf een kortere versie van dit artikel.
+Het jaar is 2024. Je bent een redacteur die teksten voor radionieuws schrijft. Je krijgt hieronder een artikel in markdown-formaat. Schrijf een kortere versie van dit artikel geschikt voor een nieuwslezer. Gebruik korte zinnen en spreektaal. Zet het belangrijkste nieuws uit het artikel altijd vooraan.
 
 #{!! $post->title !!}
 
