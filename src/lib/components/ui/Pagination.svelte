d4b3c740cdce38d6fb9aa7482c22c677c9c35c4d
rmens <r.mens@me.com>
2026-06-03 01:14:56 +0200

    refactor: use native pagination links

--- src/lib/components/ui/Pagination.svelte
+++ src/lib/components/ui/Pagination.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { goto } from '$app/navigation'
   import { page } from '$app/stores'
   import { ChevronLeft, ChevronRight } from '$lib/components/icons'
   import { getVisiblePages, type PaginationInfo } from '$lib/utils/pagination'
@@ -12,14 +11,14 @@
 
   const visiblePages = $derived(getVisiblePages(pagination.currentPage, pagination.totalPages))
 
-  function goToPage(pageNum: number) {
+  function pageHref(pageNum: number): string {
     const url = new URL($page.url)
     if (pageNum === 1) {
       url.searchParams.delete('page')
     } else {
       url.searchParams.set('page', String(pageNum))
     }
-    goto(url.toString(), { invalidateAll: true })
+    return `${url.pathname}${url.search}${url.hash}`
   }
 </script>
 
@@ -29,33 +28,46 @@
     aria-label="Paginering"
   >
     <div class="join">
-      <button
-        class="btn join-item btn-sm md:btn-md"
-        onclick={() => goToPage(pagination.currentPage - 1)}
-        disabled={!pagination.hasPrevPage}
-        aria-label="Vorige pagina"
-      >
-        <ChevronLeft
-          aria-hidden="true"
-          class="h-4 w-4"
-        />
-        <span class="hidden sm:inline">Vorige</span>
-      </button>
+      {#if pagination.hasPrevPage}
+        <a
+          class="btn join-item btn-sm md:btn-md"
+          href={pageHref(pagination.currentPage - 1)}
+          aria-label="Vorige pagina"
+        >
+          <ChevronLeft
+            aria-hidden="true"
+            class="h-4 w-4"
+          />
+          <span class="hidden sm:inline">Vorige</span>
+        </a>
+      {:else}
+        <span
+          class="btn btn-disabled join-item btn-sm md:btn-md"
+          aria-disabled="true"
+          aria-label="Vorige pagina"
+        >
+          <ChevronLeft
+            aria-hidden="true"
+            class="h-4 w-4"
+          />
+          <span class="hidden sm:inline">Vorige</span>
+        </span>
+      {/if}
 
       <div class="hidden sm:contents">
         {#each visiblePages as item, i (i)}
           {#if item === 'ellipsis'}
             <span class="btn btn-disabled join-item btn-sm md:btn-md">...</span>
           {:else}
-            <button
+            <a
               class="btn join-item btn-sm md:btn-md"
               class:btn-active={item === pagination.currentPage}
-              onclick={() => goToPage(item)}
+              href={pageHref(item)}
               aria-label="Pagina {item}"
               aria-current={item === pagination.currentPage ? 'page' : undefined}
             >
               {item}
-            </button>
+            </a>
           {/if}
         {/each}
       </div>
@@ -64,18 +76,31 @@
         {pagination.currentPage} / {pagination.totalPages}
       </span>
 
-      <button
-        class="btn join-item btn-sm md:btn-md"
-        onclick={() => goToPage(pagination.currentPage + 1)}
-        disabled={!pagination.hasNextPage}
-        aria-label="Volgende pagina"
-      >
-        <span class="hidden sm:inline">Volgende</span>
-        <ChevronRight
-          aria-hidden="true"
-          class="h-4 w-4"
-        />
-      </button>
+      {#if pagination.hasNextPage}
+        <a
+          class="btn join-item btn-sm md:btn-md"
+          href={pageHref(pagination.currentPage + 1)}
+          aria-label="Volgende pagina"
+        >
+          <span class="hidden sm:inline">Volgende</span>
+          <ChevronRight
+            aria-hidden="true"
+            class="h-4 w-4"
+          />
+        </a>
+      {:else}
+        <span
+          class="btn btn-disabled join-item btn-sm md:btn-md"
+          aria-disabled="true"
+          aria-label="Volgende pagina"
+        >
+          <span class="hidden sm:inline">Volgende</span>
+          <ChevronRight
+            aria-hidden="true"
+            class="h-4 w-4"
+          />
+        </span>
+      {/if}
     </div>
   </nav>
 {/if}
